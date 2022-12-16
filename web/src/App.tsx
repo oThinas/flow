cb1eb0dda498c9d217dee2dc5a65973add4f7756
oThinas <thiagomartinsprado@gmail.com>
2022-12-16 03:52:37 -0300

    commit inicial

---
+++ web/src/App.tsx
@@ -0,0 +1,9 @@
+import './styles/global.css'
+
+export function App() {
+  return (
+    <div className='h-[3000px] bg-red-300 flex justify-center items-center'>
+      <h1 className='text-3xl font-bold text-blue'>Thinas</h1>
+    </div>
+  );
+}
